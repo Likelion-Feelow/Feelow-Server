10bf19c4c0bc6d0cc6540be99aa4eaf2e2b9eb59
byunhm02 <byunhm02@gmail.com>
2024-07-29 23:16:22 +0900

    chore: task_emotion,sub_emotion을 전,후 감정필드로 변경

--- tasks/models.py
+++ tasks/models.py
@@ -9,5 +9,7 @@ class Tasks(models.Model):
     task_name=models.CharField(max_length=100, default='')
     task_description=models.TextField(max_length=500,null=True,blank=True)
     task_duration=models.IntegerField(default=0) #초 단위.
-    task_emotion=models.CharField(max_length=50,blank=True, null=True)
-    sub_emotion = models.TextField(blank=True, null=True)  # 서브 이모션 필드 추가
+    emotion_before=models.CharField(max_length=50,blank=True, null=True) #할 일 전 서브 이모션(하나만 선택 가능하게 할 경우)
+    emotion_after=models.CharField(max_length=50,blank=True, null=True) #할 일 마친 후 서브 이모션(하나만 선택 가능하게 할 경우)
+    
+    #task_emotion = models.TextField(blank=True, null=True)  # 서브 이모션 여러개 선택 가능하게 할 경우 text필드 사용해 리스트 받아오기
