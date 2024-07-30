21407da51c05f94df05e6c994fac8ff079240210
luckyyy <oyslucy@naver.com>
2024-07-30 21:33:20 +0900

    [Chore/#1] delete duplicate class

--- timers/serializers.py
+++ timers/serializers.py
@@ -5,11 +5,3 @@ class TimerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timers
         fields = '__all__'
-
-class EmotionUpdateSerializer(serializers.Serializer):
-    current_emotion = serializers.CharField(max_length=50)
-
-    def update(self, instance, validated_data):
-        instance.current_emotion = validated_data.get('emotion', instance.current_emotion)
-        instance.save()
-        return instance
