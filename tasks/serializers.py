7a43c86398b2e7da598e968412b911d6cecd3273
luckyyy <oyslucy@naver.com>
2024-08-05 15:39:36 +0900

    [Fix] debug용 코드 및 불필요한 코드 삭제

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -41,20 +41,7 @@ class CreateTaskSerializer(serializers.ModelSerializer):
     
     
 class EmotionUpdateSerializer(serializers.ModelSerializer):
-    '''
-    class Meta:
-        model = Tasks
-        fields = ["task_name", "current_emotion", "changed_emotion"]
 
-    def update(self, instance, validated_data):
-        instance.task_name = validated_data.get('task_name', instance.task_name)
-        instance.current_emotion = validated_data.get('current_emotion', instance.current_emotion)
-        instance.changed_emotion = validated_data.get('changed_emotion', instance.changed_emotion)
-        instance.save()
-        self.update_superior_emotion(instance.calendar)
-        return instance
-    '''
-    
     #통계를 위해 수정함
     focus_time = serializers.IntegerField(required=False)
     break_time = serializers.IntegerField(required=False)
@@ -73,21 +60,14 @@ class EmotionUpdateSerializer(serializers.ModelSerializer):
 
     def update_superior_emotion(self, calendar):
         tasks_for_day = Tasks.objects.filter(user=calendar.user, calendar=calendar, current_emotion__isnull=False).exclude(current_emotion='')
-        # Print current_emotion for debugging
-        print("Tasks and Emotions:")
         for task in tasks_for_day:
             print(f"Task: {task.task_name}, Emotion: {task.current_emotion}")
 
         emotion_categories = [Emotions.get_emotion_category(task.current_emotion) for task in tasks_for_day if task.current_emotion]
-        
-        # Print emotion categories for debugging
-        print(f"Emotion Categories: {emotion_categories}")
 
         if emotion_categories:
             emotion_counts = Counter(emotion_categories)
             most_common_emotion, _ = emotion_counts.most_common(1)[0]
-            # Print most common emotion for debugging
-            print(f"Most Common Emotion: {most_common_emotion}")
             calendar.superior_emotion = most_common_emotion
         else:
             calendar.superior_emotion = None
